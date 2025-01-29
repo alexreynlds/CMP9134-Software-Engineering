9ffc459ee3bb7721e29ad1fac1cca27c47319a6f
alexreynlds <ajrey@live.co.uk>
2025-01-29 15:20:14 +0000

    + Moved sensitive info to .env file

--- software-engineering/src/firebase/config.js
+++ software-engineering/src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWIUEdE1hwVLhmV3KdpjCcac2bYoOG4ME",
-  authDomain: "software-engineering-f862a.firebaseapp.com",
-  projectId: "software-engineering-f862a",
-  storageBucket: "software-engineering-f862a.firebasestorage.app",
-  messagingSenderId: "1037817438226",
-  appId: "1:1037817438226:web:2aad83c49d3f2eb54038cb",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
